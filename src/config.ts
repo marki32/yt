3793bb5bc744c064e3349e67c2aca17e55c5c58c
marki32 <idlumino175@gmail.com>
2025-01-14 17:56:05 +0530

    Initial commit

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.PROD 
-  ? 'https://yt-zq8kknk.vercel.app'  // This will be updated after backend deployment
+  ? 'https://yt-zq8kknk-8392wmekf-mas-projects-98328f16.vercel.app'  // Backend URL
   : 'http://localhost:3001';
